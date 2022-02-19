12f61eaee0d66bb06e0a8f95c2e2885a70eeb2dc
daawidziu <daawidziu@int.pl>
2022-02-19 21:42:26 +0100

    Add JWT-Extended and BCrypt to application

--- application/__init__.py
+++ application/__init__.py
@@ -1,11 +1,15 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
+from flask_jwt_extended import JWTManager
+from flask_bcrypt import Bcrypt
 
 import config
 
 db = SQLAlchemy()
 ma = Marshmallow()
+jwt = JWTManager()
+bc = Bcrypt()
 
 
 def init_app(config_file: object | str = config.DevelopmentConfig) -> Flask:
@@ -14,5 +18,7 @@ def init_app(config_file: object | str = config.DevelopmentConfig) -> Flask:
 
     db.init_app(app)
     ma.init_app(app)
+    jwt.init_app(app)
+    bc.init_app(app)
 
     return app
