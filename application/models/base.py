c314786dd140a8cfb5fd0af593249652af85f1e4
daawidziu <daawidziu@int.pl>
2022-02-20 23:12:09 +0100

    Create base data model

---
+++ application/models/base.py
@@ -0,0 +1,56 @@
+from sqlalchemy.orm import declared_attr
+from sqlalchemy.exc import IntegrityError, SQLAlchemyError
+
+from application import db
+
+
+class BaseModel(db.Model):
+    __abstract__ = True
+
+    # Base Information
+    id = db.Column(db.Integer, primary_key=True)
+    created_at = db.Column(db.DateTime, nullable=False, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, nullable=False, server_default=db.func.now(), onupdate=db.func.now())
+
+    @declared_attr
+    def __tablename__(cls):
+        return cls.__name__.lower()
+
+    @classmethod
+    def get(cls, **kwargs) -> 'BaseModel':
+        return cls.query.filter_by(**kwargs).first()
+
+    @classmethod
+    def filter_by(cls, limit=100, **filters) -> list['BaseModel']:
+        return cls.query.filter_by(**filters).limit(limit).all()
+
+    def update(self, new_data: dict) -> None:
+        try:
+            for key, value in new_data.items():
+                setattr(self, key, value)
+            db.session.commit()
+        except SQLAlchemyError as e:
+            db.session.rollback()
+            print(e)
+
+    def save(self) -> None:
+        try:
+            db.session.add(self)
+            db.session.commit()
+        except SQLAlchemyError as e:
+            db.session.rollback()
+            print(e)
+        except IntegrityError as e:
+            db.session.rollback()
+            print(e)
+
+    def delete(self) -> None:
+        try:
+            db.session.delete(self)
+            db.session.commit()
+        except SQLAlchemyError as e:
+            db.session.rollback()
+            print(e)
+
+    def __repr__(self) -> str:
+        return str(self.__dict__)
