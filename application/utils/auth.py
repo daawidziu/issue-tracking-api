46e1e8cab0284b90cac08805bfe3c33578492794
daawidziu <daawidziu@int.pl>
2022-03-10 20:05:59 +0100

    Move auth handlers to separate file

--- application/utils/auth.py
+++ application/utils/auth.py
@@ -1,28 +1,6 @@
 from itsdangerous import URLSafeTimedSerializer, BadSignature, SignatureExpired
 from flask import current_app
 
-from application import jwt
-from application.models import Account
-
-
-@jwt.token_in_blocklist_loader
-def check_if_token_revoked(jwt_header, jwt_payload) -> bool:
-    jti = jwt_payload['jti']
-    redis = current_app.redis
-    token = redis.get(jti)
-    return token is not None
-
-
-@jwt.user_identity_loader
-def user_identity_lookup(user: Account) -> str:
-    return user.public_id
-
-
-@jwt.user_lookup_loader
-def user_lookup_callback(_jwt_header, jwt_data) -> Account:
-    identity = jwt_data['sub']
-    return Account.get(public_id=identity)
-
 
 def generate_confirmation_token(email: str) -> str | bytes:
     """Generate email account confirmation token"""
