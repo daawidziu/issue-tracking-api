d3b93ffb62d87ed861d5ca9fa4097b7381a21a4f
daawidziu <daawidziu@int.pl>
2022-03-10 20:12:28 +0100

    Add Flask Mail

--- application/__init__.py
+++ application/__init__.py
@@ -5,6 +5,7 @@ from flask_marshmallow import Marshmallow
 from flask_jwt_extended import JWTManager
 from flask_bcrypt import Bcrypt
 from flask_migrate import Migrate
+from flask_mail import Mail
 from redis import Redis
 
 
@@ -13,6 +14,7 @@ ma = Marshmallow()
 jwt = JWTManager()
 bc = Bcrypt()
 migrate = Migrate()
+mail = Mail()
 
 
 def init_app(config_file: object | str = 'config.ProductionConfig') -> Flask:
@@ -27,13 +29,13 @@ def init_app(config_file: object | str = 'config.ProductionConfig') -> Flask:
     jwt.init_app(app)
     bc.init_app(app)
     migrate.init_app(app, db)
-
-    @app.before_first_request
-    def db_create_all():
-        db.create_all()
+    mail.init_app(app)
 
     with app.app_context():
+        from application.utils import register_handlers
         from application.api import api_bp
+
+        register_handlers(app, db, jwt)
         app.register_blueprint(api_bp)
 
     return app
