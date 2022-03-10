9200772f4253b5e1f120a8538c4c48df651e05e2
daawidziu <daawidziu@int.pl>
2022-03-10 20:12:45 +0100

    Change in email provider settings

--- config.py
+++ config.py
@@ -10,15 +10,17 @@ class Config:
     DEBUG = False
     TESTING = False
     PASSWORD_LOG_ROUNDS = os.getenv("PASSWORD_LOG_ROUNDS", 4)
-    AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
-    AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
-    SES_REGION = os.getenv("SES_REGION")
-    SES_EMAIL_SOURCE = os.getenv("SES_EMAIL_SOURCE")
     ACCESS_EXPIRES = timedelta(minutes=float(os.getenv("ACCESS_EXPIRES", 1)))
     EMAIL_MAX_AGE = int(os.getenv("EMAIL_MAX_AGE", 1))
     REDIS_URL = os.getenv("REDIS_URL", "redis://")
     JWT_TOKEN_LOCATION = ['cookies']
     JWT_COOKIE_SECURE = True
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 465
+    MAIL_USERNAME = os.getenv('MAIL_USERNAME')
+    MAIL_PASSWORD = os.getenv('MAIL_PASSWORD')
+    MAIL_USE_TLS = False
+    MAIL_USE_SSL = True
 
 
 class ProductionConfig(Config):
