cbff050d154c3e8546f27eda66ecd49345698e88
daawidziu <daawidziu@int.pl>
2022-03-20 12:33:31 +0100

    Add GET /projects/stats

--- application/resources/projects.py
+++ application/resources/projects.py
@@ -3,21 +3,53 @@ from flask import request
 from flask_restx import Namespace, fields, Resource, abort
 from flask_jwt_extended import jwt_required
 
-from application.models import Project
+from application.models import Project, Issue
 from application.schemas import ProjectSchema
 
 schema = ProjectSchema()
 schema_list = ProjectSchema(many=True)
 projects_ns = Namespace("projects", description="Projects CRUD operations")
 
-model = projects_ns.model("Project", {
-    "name": fields.String("Project name"),
-    "description": fields.String("Project description"),
+model = projects_ns.model('Project', {
+    'name': fields.String('Project name'),
+    'description': fields.String('Project description'),
     'created_at': fields.DateTime('User creation date', readonly=True),
     'updated_at': fields.DateTime('User update date', readonly=True),
-    "id": fields.Integer(readonly=True)
+    'id': fields.Integer(readonly=True),
+    'url': fields.Url()
 })
 
+model_stats = projects_ns.model('ProjectStats', {
+    'name': fields.String('Project name'),
+    'description': fields.String('Project description'),
+    'created_at': fields.DateTime('User creation date', readonly=True),
+    'updated_at': fields.DateTime('User update date', readonly=True),
+    'id': fields.Integer(readonly=True),
+    'open_issues': fields.Integer(),
+    'wip_issues': fields.Integer(),
+    'closed_issues': fields.Integer(),
+    'url': fields.Url()
+})
+
+
+@projects_ns.route('/stats')
+@projects_ns.response(int(HTTPStatus.BAD_REQUEST), 'Validation Error')
+@projects_ns.response(int(HTTPStatus.INTERNAL_SERVER_ERROR), 'Internal server error')
+class ProjectsStats(Resource):
+
+    @jwt_required(optional=True)
+    @projects_ns.response(int(HTTPStatus.OK), 'Get all Projects with Statistics', fields.List(fields.Nested(model)))
+    def get(self):
+        projects = Project.filter_by()
+        data = []
+        for project in projects:
+            data.append({
+                **ProjectSchema().dump(project),
+                'open_issues': Issue.count(project_id=project.id, status='open'),
+                'closed_issue': Issue.count(project_id=project.id, status='closed'),
+                'wip_issues': Issue.count(project_id=project.id, status='wip'),
+            })
+        return data
 
 @projects_ns.route('/')
 @projects_ns.response(int(HTTPStatus.BAD_REQUEST), 'Validation Error')
